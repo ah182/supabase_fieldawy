982ceed7811812195d76dded8af06efd9e0986e2
ah182 <ah3181997@gmail.com>
2025-12-28 20:13:00 +0200

    build] 23

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 
     defaultConfig {
         applicationId = "com.fieldawy.store"
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
